00a716acfb26e1321f183bc0f84afe4b6a4982ee
Antony <kuzmi495@mail.ru>
2022-01-08 21:22:14 +0300

    feat: add task solution in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    n = Math.abs(n);
+    let numbToString = String(n);
+    let tmpString = "";
+    for (let i = numbToString.length - 1; i >= 0 ; i--) {
+        tmpString += numbToString[i];
+    }
+    return tmpString;
 }
